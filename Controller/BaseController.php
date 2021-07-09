6935134fbc73c6809e29b1c4f108a00c7fbead69
Thomas Pieters <pieters.tg@gmail.com>
2021-07-09 15:18:58 +0200

    apiCLient cleanup

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -1,30 +1,20 @@
 <?php
 
 
+use GuzzleHttp\Exception\GuzzleException;
+
 class BaseController
 {
+    /**
+     * @throws GuzzleException
+     */
     public function index(ApiClient $client): void
     {
         //5fc2680e4798be363b7961342becef0a62509f14 key for John Testington I
         $key = '5fc2680e4798be363b7961342becef0a62509f14';
 
-         $client->getView($key);
-
+//        $client->postOrder($key, "pickup \n  move 2 2");
+         $client->getGrid($key);
 
     }
-
-    public function getKey():void
-    {
-        $client = new GuzzleHttp\Client();
-        $request = new \GuzzleHttp\Psr7\Request('GET', 'game-the-warehouse.herokuapp.com?mode=start&name=John_Testington_I&easymode=true');
-        $promise = $client->sendAsync($request)->then(function ($response) {
-            echo 'I completed! ' . $response->getBody();
-        });
-        $promise->wait();
-    }
-
-
-
-
-
 }
--- Model/ApiClient.php
+++ Model/ApiClient.php
@@ -2,13 +2,19 @@
 
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class ApiClient
 {
     private Client $client;
-    const BASE_PATH = 'game-the-warehouse.herokuapp.com';
-    const KEY_REQUEST = '?mode=start&name=John_Testington_I&easymode=true';
-    const VIEW_REQUEST = '?mode=view&key=';
+    private const BASE_PATH = 'game-the-warehouse.herokuapp.com';
+    private const INGREDIENTS_REQUEST = '?mode=ingredients&key=';
+    private const ORDER_REQUEST = '?mode=orders&key=';
+    private const STATE_REQUEST = '?mode=state&key=';
+    private const GRID_REQUEST = '?mode=grid&key=';
+    private const VIEW_REQUEST = '?mode=view&key=';
+    private const DO_TURN = '?mode=doturn&key=';
+    private const ROBOTS_REQUEST = '?mode=robots&key=';
 
     /**
      * ApiClient constructor.
@@ -18,12 +24,107 @@ class ApiClient
         $this->client = new GuzzleHttp\Client();
     }
 
-    public function getView(string $key): string
+    /**
+     * @throws GuzzleException
+     * @throws Exception
+     */
+    private function getRequest(string $type, string $key):array
     {
-        $request = new \GuzzleHttp\Psr7\Request('GET', self::BASE_PATH.self::VIEW_REQUEST.$key);
-        $promise = $this->client->sendAsync($request)->then(function ($response) {
-        echo $response->getBody()->getContents();
-        });
-        $promise->wait();
+        try {
+            $response = $this->client->get(self::BASE_PATH.$type.$key);
+            return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        }catch (Exception){
+            throw new Exception("API request failed");
+        }
     }
+
+    /**
+     * @throws GuzzleException
+     * @throws Exception
+     */
+    public function postCommand($key, string $order):array
+    {
+        try {
+            $response = $this->client->post(self::BASE_PATH.self::DO_TURN.$key,["form_params" => ["input"=> $order]]);
+            return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        }catch (Exception)
+        {
+            throw new Exception("Command could not be sent");
+        }
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws Exception
+     */
+    public function getView(string $key): void
+    {
+        try {
+            $response = $this->client->get(self::BASE_PATH.self::VIEW_REQUEST.$key);
+            echo $response->getBody()->getContents();
+        }catch (Exception){
+            throw new Exception("Fetching of view failed");
+        }
+
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getOrders(string $key): void
+    {
+        $ordersArray = $this->getRequest(self::ORDER_REQUEST , $key);
+        var_dump($ordersArray[0]);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getRobots($key):void
+    {
+        $robotArray = $this->getRequest(self::ROBOTS_REQUEST , $key);
+        var_dump($robotArray);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getIngredients($key):void
+    {
+        $ingredientsArray = $this->getRequest(self::INGREDIENTS_REQUEST , $key);
+        var_dump($ingredientsArray);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getGrid($key):void
+    {
+        $gridArray = $this->getRequest(self::GRID_REQUEST , $key);
+        var_dump($gridArray);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getState($key):void
+    {
+        $stateArray = $this->getRequest(self::STATE_REQUEST , $key);
+        var_dump($stateArray);
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws Exception
+     */
+    public function getNewGameKey(string $newPlayerName):array
+    {
+        try {
+            $response = $this->client->get("?mode=start&name=${$newPlayerName}&easymode=true");
+            return json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        }catch (Exception){
+            throw new Exception("Game-key request failed");
+        }
+    }
+
 }
